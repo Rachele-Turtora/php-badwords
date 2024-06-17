5598e1bc9872dac02e22d538682a731884c02a29
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-17 17:04:31 +0200

    [UPD] style (part 2)

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <h1>Censura il tuo testo</h1>
             <form action="text.php" method="GET">
-                <textarea name="text"></textarea>
+                <textarea name="text" placeholder="Inserisci il testo"></textarea>
                 <div class="input-container">
                     <label for="badword">Censura: </label>
                     <input type="text" name="badword" placeholder="Scrivi una parola" id="badword">
@@ -21,4 +23,5 @@
         </div>
     </div>
 </body>
+
 </html>
