1bf95f1c1769b8173d1a8c3c1d5b70c149f30c44
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-17 14:21:45 +0200

    [UPD] form logic

--- text.php
+++ text.php
@@ -1,6 +1,11 @@
 <?php
     $text = $_GET["text"];
     $bad_word = $_GET["badword"];
+
+    $text_length1 = strlen($text);
+
+    $text2 = str_replace($bad_word, "***", $text);
+    $text_length2 = strlen($text2);
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +16,12 @@
     <title>Document</title>
 </head>
 <body>
+    <h2>Il tuo testo:</h2>
     <p> <?php echo $text ?></p>
-    <p> <?php echo $bad_word ?></p>
+    <p>Lunghezza: <?php echo $text_length1 ?></p>
+
+    <h2>Il tuo testo censurato:</h2>
+    <p> <?php echo $text2 ?></p>
+    <p>Lunghezza: <?php echo $text_length2 ?></p>
 </body>
 </html>
